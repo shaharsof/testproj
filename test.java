68947221dbd1225cb280fb2432bd4b85e2329cb1
shaharsof <53426467+shaharsof@users.noreply.github.com>
2023-07-26 14:41:46 +0300

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class SQLInjection
 {
      public static void main (String [] args)
 	 {
-		String accountNumber = args[1];
+		String accountNumber = args[2];
 		String query = "SELECT * FROM user_data WHERE acountNumber = " + accountNumber; 
 		
 		Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
